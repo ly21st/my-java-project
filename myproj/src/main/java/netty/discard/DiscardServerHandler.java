2c83413ecaf050e06532e1c386b6150f3f10daa2
liyuan <liyuan@agree.com.cn>
2021-12-09 10:12:35 +0800

    同步

---
+++ myproj/src/main/java/netty/discard/DiscardServerHandler.java
@@ -0,0 +1,31 @@
+package netty.discard;
+
+/**
+ * @description:
+ * @author: LiYuan
+ * @version: 1.0
+ * @create: 2021-11-11
+ **/
+import io.netty.buffer.ByteBuf;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Handles a server-side channel.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        // Discard the received data silently.
+        ((ByteBuf) msg).release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
